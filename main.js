8e2e6aaeaab1329645622c0c085aa244d86896f7
colorlessenergy <brianmunozdev@gmail.com>
2018-09-15 14:12:08 -0700

    add comments

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
+// Well, guess what? You have to build a Xbonacci function that takes a signature of X elements - and
+//  remember each next element is the sum of the last X elements - and returns the first n elements of the so 
+// seeded sequence.
+
 function Xbonacci (sig, n) {
   var len = sig.length;
 
+  // loop starting from the amount of items in the array to the amount specified
   for (let i = len; i < n; i++) {
+    // slices the array starting from 0 and everytime we iterate move starting by 1
+    // then add the whole array so we get the value that should be next the in the sequence.
     sig[i] = sig.slice(i - len).reduce(function (a, b) {
       return a + b;
     })
   }
 
-  return sig.slice(0, n)
+  // return final array
+  return sig;
 }
 
 console.log(Xbonacci([0,0,0,0,1],10));
